d943c67e5fb49eb2b863ea2aa735fd00b55d9ba0
JCohrs <jeffscohrs@gmail.com>
2017-04-22 20:46:38 -0400

    add Geofences

---
+++ app/src/main/java/com/example/android/hawkpark01/utils/GeofenceConstants.java
@@ -0,0 +1,32 @@
+package com.example.android.hawkpark01.utils;
+
+import com.google.android.gms.maps.model.LatLng;
+
+import java.util.LinkedHashMap;
+
+/**
+ * Created by jeffs on 4/22/2017.
+ */
+
+public class GeofenceConstants {
+    public void GeofenceConstants(){}
+
+    private static final LatLng carparcDiem = new LatLng(40.865314, - 74.197107);
+    private static final LatLng lot24 = new LatLng(40.866385, -74.196641);
+    private static final LatLng lot60 = new LatLng(40.872985, -74.198942);
+
+    public static final LinkedHashMap<String, LatLng> MSU_PARKING =
+            new LinkedHashMap<String, LatLng>();
+    static {
+        MSU_PARKING.put("CarParc Diem", carparcDiem);
+        MSU_PARKING.put("Lot 24", lot24);
+        MSU_PARKING.put("Lot 60", lot60);
+    }
+    public static final LinkedHashMap<String, Integer> LOT_RADIUS =
+            new LinkedHashMap<String, Integer>();
+    static {
+        LOT_RADIUS.put("CarParc Diem", 60);
+        LOT_RADIUS.put("Lot 24", 60);
+        LOT_RADIUS.put("Lot 60", 150);
+    }
+}
