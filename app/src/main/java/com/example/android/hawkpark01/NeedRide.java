023419c3a3c821a2cb8f459bcab6662c744ca445
priya <jkster@gmail.com>
2017-04-28 08:03:10 -0400

    add NeedParking and NeedRide DB models add need_ride layout add need_parking item layout for custom list item add parking lot name array and pick up location array in utils add admob provision to gradle

--- app/src/main/java/com/example/android/hawkpark01/NeedRide.java
+++ app/src/main/java/com/example/android/hawkpark01/NeedRide.java
@@ -6,6 +6,9 @@ import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Spinner;
+
+import com.example.android.hawkpark01.utils.SpaceCalculator;
 
 import java.util.List;
 
@@ -14,7 +17,8 @@ public class NeedRide extends AppCompatActivity {
     RadioGroup rg_leaving;
     RadioButton rbtn_now, rbtn_later;
     EditText et_leavingTime;
-    EditText et_parkedLot;
+    Spinner spinner_parkedLot;
+    Spinner spinner_pickup_location;
     EditText et_pickupSpot;
     RadioGroup rg_passengers;
     ListView lv_needParking;
@@ -23,5 +27,7 @@ public class NeedRide extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_need_ride);
+
+
     }
 }
